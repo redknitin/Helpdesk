13809a66205d85f627615470674ebdf0c20d5990
Nitin Reddy <redknitin@gmail.com>
2017-10-31 11:12:43 +0400

    Moved DB config, building-floor, org-dept to the app_config.default so they can be overridden in app_config.env

--- app_config.defaults.rb
+++ app_config.defaults.rb
@@ -8,6 +8,23 @@
 
 class AppConfig
   #
+  # Database settings
+  #
+  DB_URL = 'mongodb://127.0.0.1:27017/helpdesk'  #mongodb://user:password@127.0.0.1:27017/helpdesk
+
+  #
+  # Master data for the application
+  #
+  MASTER_BLDG_FLOOR = [
+    {:building => 'Building 01', :floors => ['Roof Top', '2nd Floor (2)', '1st Floor (1)', 'Ground Floor (0)', 'Lower Ground (B1/-1)', 'Basement 1 (B2/-2)', 'Basement 2 (B3/-3)']},
+    {:building => 'Building 02', :floors => ['2nd Floor (2)', '1st Floor (1)', 'Ground Floor (0)', 'Lower Ground (B1/-1)', 'Basement 1 (B2/-2)']}
+  ]
+  MASTER_ORG_DEPT = [
+    {:org => 'Apache Foundation', :dept => ['Software Development', 'Quality Control']},
+    {:org => 'Canonical', :dept => ['System Administration', 'Marketing']}
+  ]
+  
+  #
   # Email settings
   #
   # MAIL_SMTP_HOST = 'localhost'
--- app_config.rb
+++ app_config.rb
@@ -4,22 +4,6 @@
 # Use rackup and pass command-line parameters for host, port, server instead of set :bind and set :port - see runme.bat for an example
 
 class AppConfig
-  #
-  # Database settings
-  #
-  DB_URL = 'mongodb://127.0.0.1:27017/helpdesk'  #mongodb://user:password@127.0.0.1:27017/helpdesk
-
-  #
-  # Master data for the application
-  #
-  MASTER_BLDG_FLOOR = [
-    {:building => 'Building 01', :floors => ['Roof Top', '2nd Floor (2)', '1st Floor (1)', 'Ground Floor (0)', 'Lower Ground (B1/-1)', 'Basement 1 (B2/-2)', 'Basement 2 (B3/-3)']},
-    {:building => 'Building 02', :floors => ['2nd Floor (2)', '1st Floor (1)', 'Ground Floor (0)', 'Lower Ground (B1/-1)', 'Basement 1 (B2/-2)']}
-  ]
-  MASTER_ORG_DEPT = [
-    {:org => 'Apache Foundation', :dept => ['Software Development', 'Quality Control']},
-    {:org => 'Canonical', :dept => ['System Administration', 'Marketing']}
-  ]
   MASTER_ROLES = ['requester', 'helpdesk', 'admin']
   MASTER_STATUSES = ['New', 'In Progress', 'Suspended', 'Completed', 'Cancelled']
 
