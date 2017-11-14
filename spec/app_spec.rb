f35b5230fca9cc22a60aa7e291d1e5f8dea51038
Nitin Reddy <redknitin@gmail.com>
2017-11-14 17:22:35 +0400

    Added comments to the RSpec tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -14,12 +14,14 @@ describe 'Helpdesk' do
 		Helpdesk
 	end
 
+	#Check if the login page shows
 	it 'shows login' do
 		get '/login'
 		expect(last_response.status).to eq 200
 		expect(last_response.body).to include('Login')
 	end
 
+	#Perform a login with the default user account for admin
 	it 'actually logs in' do
 		post '/login', { :id => 'admin', :pw => 'admin' }
 		expect(last_response.status).to eq 302
@@ -29,22 +31,17 @@ describe 'Helpdesk' do
 		expect(last_response.body).to include('Click on the request ID to view details of the event')
 	end
 
+	#Ensure it doesn't accept an invalid password
 	it 'stops unauthorized users' do
 		post '/login', { :id => 'admin', :pw => 'notadmin' }
 		expect(last_response.status).to eq 302
 
 		get '/tickets-list'
 		expect(last_response).to be_redirect
-		# expect(last_response.status).to eq 302
-		
 		expect(last_response.location).to include('/login')
-
-		# follow_redirect!
-		# expect(last_request.url).to include('/login')
-		# puts last_response.body
-		# expect(last_response.body).to include('Login')
 	end
 
+	#Register as a new user and perform a login
 	it 'registers new users' do
 		randomstr = Array.new(10){rand(36).to_s(36)}.join.downcase
 		username = 'test_' + randomstr
